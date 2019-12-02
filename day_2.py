11844d36ffec6a6d618fff2921b5c516e802f7ad
Maks Kolman <kolman.maks@gmail.com>
2019-12-02 17:37:32 +0000

    Rewrite into a const solution

--- day_2.py
+++ day_2.py
@@ -3,10 +3,6 @@ def get_inp():
     arr = list(map(int, f.read().split(',')))
     return {i: n for i, n in enumerate(arr)}
 
-def prn(state):
-  for _, v in sorted(state.items()):
-    print(v, end=',')
-  print()
 
 def easy(noun=12, verb=2):
   state = get_inp()
@@ -15,8 +11,6 @@ def easy(noun=12, verb=2):
 
   idx = 0
   while state[idx] != 99:
-    # print(idx)
-    # prn(state)
     a, b, c = (state[idx+i] for i in range(1, 4))
     if state[idx] == 1:
       state[c] = state[a] + state[b]
@@ -27,11 +21,30 @@ def easy(noun=12, verb=2):
     idx += 4
   return state[0]
 
+def easy2(noun=12, verb=2):
+  return noun*405000 + verb + 250673
+
+def test_easy():
+  for n in range(100):
+    for v in range(100):
+      assert easy(n, v) == easy2(n, v), (n, v, easy(n, v), easy2(n, v))
+
 def hard():
   for noun in range(100):
     for verb in range(100):
       if easy(noun, verb) == 19690720:
         return 100*noun + verb
 
-print(easy())
-print(hard())
+def hard2():
+  r = 19690720 - 250673
+  verb = r % 405000
+  noun = r // 405000
+  return 100*noun + verb
+
+def test_hard():
+  assert hard() == hard2()
+
+print(easy2())
+print(hard2())
+test_easy()
+test_hard()
