2e5d62b8081bd6ef904955613e8772d8fa9bddad
Maks Kolman <makskolman@google.com>
2019-12-02 15:21:37 +0000

    Day 1

---
+++ day_1.py
@@ -0,0 +1,22 @@
+def get_inp():
+  with open('input/day_1.txt') as f:
+    for line in f:
+      yield int(line)
+
+def easy():
+  result = 0
+  for inp in get_inp():
+    result += inp // 3 - 2
+  return result
+
+def hard():
+  result = 0
+  for inp in get_inp():
+    inp = inp // 3 - 2
+    while inp > 0:
+      result += inp
+      inp = inp // 3 - 2
+  return result
+
+print(easy())
+print(hard())
