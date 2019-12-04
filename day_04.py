245bf780cc4150c56cf269a3e906c614ab64ee57
Maks Kolman <kolman.maks@gmail.com>
2019-12-04 10:40:27 +0000

    Speed up by only looking at numbers with nondecreesing digits

--- day_04.py
+++ day_04.py
@@ -2,24 +2,29 @@ def get_inp():
   with open('input/day_04.txt') as f:
     return map(int, f.read().split('-'))
 
+def num_gen(depth=6, prev=""):
+  if depth == 0:
+    yield prev
+  else:
+    low = int(prev[-1]) if prev else 1
+    for d in range(low, 10):
+      for r in num_gen(depth-1, prev + str(d)):
+        yield r
+
 def easy():
   low, high = get_inp()
   cnt = 0
-  for n in range(low, high):
-    s = str(n)
-    inc = all(a <= b for a, b in zip(s, s[1:]))
+  for s in num_gen():
     rep = any(a == b for a, b in zip(s, s[1:]))
-    cnt += inc and rep
+    cnt += rep and low < int(s) < high
   return cnt
 
 def hard():
   low, high = get_inp()
   cnt = 0
-  for n in range(low, high):
-    s = str(n)
-    inc = all(a <= b for a, b in zip(s, s[1:]))
+  for s in num_gen():
     rep = any(s.count(c) == 2 for c in s)
-    cnt += inc and rep
+    cnt += rep and low < int(s) < high
   return cnt
 
 print(easy())
