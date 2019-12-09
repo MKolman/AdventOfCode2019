8c027d6e83b7bb2f67e120fe150b911c12109491
Maks Kolman <kolman.maks@gmail.com>
2019-12-09 08:38:57 +0000

    Add timer to test.py

--- test.py
+++ test.py
@@ -1,13 +1,18 @@
+import time
+
 def test():
+  start = time.time()
   for d in range(1, 26):
-    result = 'Day {:02d} '.format(d)
+    result = 'Day {:2d} '.format(d)
     try:
+      t = time.time()
       solution = __import__('day_{:02d}'.format(d))
       e, h = solution.test()
-      result += '{} {}'.format(' ✓'[e], ' ✓'[h])
+      result += '{} {} ({}s)'.format(' ✓'[e], ' ✓'[h], round(time.time() - t, 3))
     except ModuleNotFoundError:
       result += '✗ ✗'
-    print('{}'.format(result))
+    print(result)
+  print('[Finished in {}s]'.format(round(time.time() - start, 2)))
 
 if __name__ == '__main__':
   test()
